78e44ce2e1e3b941b7d48de769977416008874bf
Alexander <asaulovalex@gmail.com>
2023-04-20 18:03:59 +0300

    added cors origin *

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import fs from "fs";
-// const cors = require("cors");
+const cors = require("cors");
 
 const port = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+  })
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
